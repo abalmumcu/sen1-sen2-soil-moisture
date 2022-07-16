ac7b13ceb9f37416f1e2d73d069554e164b87c47
abalmumcu <alperbalmumcu@gmail.com>
2022-07-17 00:57:08 +0300

    main update

--- GAN/main.py
+++ GAN/main.py
@@ -1,7 +1,11 @@
 from GAN.GAN_model import gan_model
-from GAN.utils import image_load
+from GAN.utils import dataset
 
+sentinel_1_path= "./extracted_images/Sentinel1/Sentinel1_crop_"
+sentinel_2_path= "./extracted_images/Sentinel2/Sentinel2_crop_"
 
+Sen1 = dataset(sentinel_1_path,sentinel_type= 1)
+Sen2,S2_Features = dataset(sentinel_2_path,sentinel_type= 2)
 
 image_shape = (256,256,3)
 
@@ -12,4 +16,4 @@ d_model_B = gan_model.define_discriminator(image_shape)
 c_model_AtoB = gan_model.define_composite_model(g_model_AtoB, d_model_B, g_model_BtoA, image_shape)
 c_model_BtoA = gan_model.define_composite_model(g_model_BtoA, d_model_A, g_model_AtoB, image_shape)
 
-gan_model.train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA,S2,S1)
+gan_model.train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA,Sen2,Sen1)
